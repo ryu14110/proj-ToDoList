3e99c628dab365e4d96f8132d8a9585d3294a4b3
ryu14110 <119416694+ryu14110@users.noreply.github.com>
2023-03-04 14:57:35 +0900

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
 // const bgImage = document.createElement("img");
 
-// bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `${chosenImage}`;
 
 // // document.body.appendChild(bgImage);
 // document.body.style.backgroundImage=bgImage;
