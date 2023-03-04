876b5d040ceb47c78528de20f186f726400b5486
ryu14110 <119416694+ryu14110@users.noreply.github.com>
2023-03-04 15:08:47 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,19 +1,13 @@
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
 // const chosenImage = images[Math.floor(Math.random() * images.length)];
-
 // const bgImage = document.createElement("img");
-
 // bgImage.src = `${chosenImage}`;
-
 // // document.body.appendChild(bgImage);
 // document.body.style.backgroundImage=bgImage;
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-
 // 배경 이미지 넣는 형식 url('img주소')
 const bgImage = `url(${chosenImage})`
-
 // body에 추가
-document.body.style.backgroundImage = bgImage
+document.body.style.backgroundImage = bgImage;
