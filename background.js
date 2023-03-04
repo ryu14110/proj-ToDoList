cb172c9c1900b7d1d1583fc9baf9485f232762e3
ryu14110 <119416694+ryu14110@users.noreply.github.com>
2023-03-04 15:01:36 +0900

    Update background.js

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 
 // 배경 이미지 넣는 형식 url('img주소')
-const bgImage = `url(img/${chosenImage})`
+const bgImage = `url(${chosenImage})`
 
 // body에 추가
 document.body.style.backgroundImage = bgImage
